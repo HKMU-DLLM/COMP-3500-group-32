5e83b05ca3c5a09aaab8eea7b89e87d90a56a00f
Kenny3311 <laikenny195@gmail.com>
2025-11-12 16:18:52 +0800

    Refactor menu and payment views to enhance item rendering and order summary display

--- routes/customer.js
+++ routes/customer.js
@@ -16,21 +16,39 @@ router.get("/", (req, res) => {
 
 // Menu page
 router.get("/menu/:id", (req, res) => {
-    const id = req.params.id; 
+	const id = req.params.id;
 
-    try {
-        const stmt = db.prepare("SELECT * FROM menus WHERE restaurant = ?");
-        const menus = stmt.all(id); 
+	try {
+		const stmt = db.prepare("SELECT * FROM menus WHERE restaurant = ?");
+		const menus = stmt.all(id);
 		console.log(menus);
 
-        res.render("customer/menu", { menus });
-    } catch (err) {
-        console.error(err);
-        res.status(500).send("Database error");
-    }
+		res.render("customer/menu", { menus });
+	} catch (err) {
+		console.error(err);
+		res.status(500).send("Database error");
+	}
 });
 
+router.post("/pay", (req, res) => {
+	const { orderDetails } = req.body;
+	try {
+		const ids = Object.keys(orderDetails);
+		const placeholders = ids.map(() => "?").join(",");
+		const stmt = db.prepare(`SELECT * FROM menus WHERE id IN (${placeholders})`);
+		const menuItems = stmt.all(...ids.map((id) => parseInt(id)));
 
+		const mergedItems = menuItems.map((item) => ({
+			...item,
+			quantity: orderDetails[item.id.toString()] || 0,
+		}));
+
+		console.log(mergedItems);
+		res.render("customer/payment", { mergedItems });
+	} catch (err) {
+		console.error(err);
+		res.status(500).send("Database error");
+	}
+});
 
 module.exports = router;
-
