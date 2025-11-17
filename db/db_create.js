9b6ba88d7880579b7eee96d7c1e66e60a7f6d0f8
Kenny3311 <laikenny195@gmail.com>
2025-11-18 01:49:34 +0800

    change: add isPaid in orders table

--- db/db_create.js
+++ db/db_create.js
@@ -38,15 +38,17 @@ CREATE TABLE orders (
     id                   INTEGER  PRIMARY KEY AUTOINCREMENT,
     customer_name        TEXT,
     customer_address     TEXT     NOT NULL,
+    isPaid               INTEGER  NOT NULL
+                                    CHECK (isPaid IN (0, 1) ),
     created_at           DATETIME DEFAULT CURRENT_TIMESTAMP,
-    rider_name           TEXT,
     restaurant_completed INTEGER  DEFAULT (0) 
                                     CHECK (restaurant_completed IN (0, 1) ) 
                                     NOT NULL,
     restaurant_name      TEXT     NOT NULL,
     restaurant_address   TEXT     NOT NULL,
-    distance_miles       INTEGER,
+    distance_m           INTEGER,
     context              TEXT,
+    rider_name           TEXT,
     FOREIGN KEY (
         customer_name
     )
